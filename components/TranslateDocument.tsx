f330c62d10cc6dd427d7606c37618d0d333af159
AmxnMehra <amanmehra0112233@gmail.com>
2025-09-12 07:43:10 +0530

    added the openAI for tranlation

--- components/TranslateDocument.tsx
+++ components/TranslateDocument.tsx
@@ -20,6 +20,9 @@ import {
 
 import { Button } from "./ui/button";
 import { useState, useTransition } from "react";
+import { toast } from "sonner";
+import { BotIcon } from "lucide-react";
+import Markdown from "react-markdown";
 
 type Language =
   | "english"
@@ -58,6 +61,27 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
 
     startTransition(async () => {
       // TODO: implement translation logic here
+      const documentData = doc.get("document-store").toJSON();
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/translateDocument`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            documentData,
+            targetLant: languages,
+          }),
+        }
+      );
+
+      if (res.ok) {
+        const { translate_text } = await res.json();
+        setSummary(translate_text);
+        toast.success("Translated Summary successfully");
+      }
     });
   };
 
@@ -77,6 +101,17 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
 
           {question && <p className="mt-5 text-gray-500">Q: {question}</p>}
         </DialogHeader>
+        {summary && (
+          <div className="flex flex-col items-start max-h-96 overflow-scroll gap-2 p-5 bg-gray-100">
+            <div className="flex">
+              <BotIcon className="w-10 flex-shrink-0" />
+              <p className="font-bold">
+                GPT {isPending ? "is thinking..." : "Says:"}
+              </p>
+            </div>
+            <p>{isPending ? "Thinking..." : <Markdown>{summary}</Markdown>}</p>
+          </div>
+        )}
 
         <form className="flex gap-2" onSubmit={handleAskQuestion}>
           <Select
@@ -105,4 +140,3 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
 }
 
 export default TranslateDocument;
-
