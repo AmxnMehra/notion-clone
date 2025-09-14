9c3e7c968a92e5bcdd30a9c544d5ef187935d7dc
AmxnMehra <amanmehra0112233@gmail.com>
2025-09-14 21:10:56 +0530

    fix the layout.tsx

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -1,13 +1,18 @@
 import RoomProvider from "@/components/RoomProvider";
 import { auth } from "@clerk/nextjs/server";
 
-export default async function DocLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { id: string };
-}) {
+export default async function DocLayout(
+  props: {
+    children: React.ReactNode;
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
+
+  const {
+    children
+  } = props;
+
   // Protect the route
   await auth.protect();
 
